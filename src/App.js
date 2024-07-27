2dffa3b642106cc82b3c539c624ad3f154abe073
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-07-27 19:53:24 +0330

    change browser tab name

--- src/App.js
+++ src/App.js
@@ -291,7 +291,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       document.title = `Movie | ${title}`;
 
       return function () {
-        document.title = "WatchWise";
+        document.title = "Watch Wise";
         // console.log(`Clean up effect for movie ${title}`);
       };
     },
